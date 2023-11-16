5686da638f8d15e367824b2c670a5bfdd831a37f
Robbert van Caem <robbertvancaem@gmail.com>
2023-11-16 17:15:50 +0100

    Update year and anonimize data

--- pages/index.js
+++ pages/index.js
@@ -12,17 +12,17 @@ export default function Home({
   return (
     <div className="w-full p-16 text-white bg-black min-h-screen leading-6">
       <Head>
-        <title>OOGT - Reports</title>
+        <title>Goals dashboard</title>
       </Head>
-      <h1 className="text-4xl text-center mb-8 font-bold">OOGT Reports</h1>
+      <h1 className="text-4xl text-center mb-8 font-bold">Goals dashboard</h1>
       <div className="text-center mb-16 text-gray-500">
         <i>Last update: </i>
         {lastUpdate}
       </div>
-      <h2 className="text-xl mb-8 font-bold">Goal #1: Revenue 2021</h2>
+      <h2 className="text-xl mb-8 font-bold">Goal #1: Revenue 2023</h2>
       <p className="text-sm">
         Main target is to have <strong>&euro;150.000</strong> in{" "}
-        <strong>new</strong> revenue by the end of 2021. The way this is
+        <strong>new</strong> revenue by the end of 2023. The way this is
         calculated in the graph below is by summing up the values of all tasks
         created after week 34 in Asana which are currently in the{" "}
         <i>Closed Won</i> section of the Sales pipeline.
@@ -46,8 +46,8 @@ export default function Home({
         Goal #2: Getting to 5 billable hours per team member per day.
       </h2>
       <p className="text-sm">
-        For every staff member that is billable (being Hulsman, Michiel, Brent,
-        Stoop (half of his time) and Robbert), we agreed on the ambition of
+        For every staff member that is billable (being Jane Doe, Brent Rogers, Harvey Dent,
+        Batman (half of his time) and The Joker), we agreed on the ambition of
         achieving a minimum of 5 billable hours per day. (red bar).
       </p>
       <BillableChart data={billabillityGraphData} />
